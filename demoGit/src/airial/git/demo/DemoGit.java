954cd8776a1847edc3ca55247ee6344f395ec2fe
cmoreau <cyril.moreau@airial.com>
2011-10-04 17:16:16 +0200

    modification du fichier pour test

--- demoGit/src/airial/git/demo/DemoGit.java
+++ demoGit/src/airial/git/demo/DemoGit.java
@@ -1,7 +1,8 @@
 package airial.git.demo;
 
 public class DemoGit {
-public static void main(String[] args) {
-	System.out.println("Hello World");
-}
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Modification du fichier !");
+	}
 }
