3eccee794978af3f1fd43744f4bb89474aecb0ba
cmoreau <cyril.moreau@airial.com>
2011-10-05 17:38:57 +0200

    ajout d'un programme important

---
+++ demoGit/src/airial/git/demo/DemoSecondFichier.java
@@ -0,0 +1,19 @@
+package airial.git.demo;
+
+import java.util.Calendar;
+
+public class DemoSecondFichier {
+public static void main(String[] args) {
+	int date = Calendar.getInstance().get(Calendar.MONTH);
+	switch (date) {
+	case 1:
+		System.out.println("Bonne année !");
+		break;
+
+	default:
+		System.out.println("Et au revoir...");
+		break;
+	}
+	
+}
+}
