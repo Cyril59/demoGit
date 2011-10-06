bcd0fe1644021314cdd81fe592462ea7ff1a97df
cmoreau <cyril.moreau@airial.com>
2011-10-06 10:07:47 +0200

    format

--- demoGit/src/airial/git/demo/DemoTransverse.java
+++ demoGit/src/airial/git/demo/DemoTransverse.java
@@ -1,7 +1,7 @@
 package airial.git.demo;
 
 public class DemoTransverse {
-public static void main(String[] args) {
-	System.out.println("Ca sert toujours...");
-}
+	public static void main(String[] args) {
+		System.out.println("Ca sert toujours...");
+	}
 }
