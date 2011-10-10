db7d34841c6190dbeb0e971acd9182c0e5a8f542
cmoreau <cyril.moreau@airial.com>
2011-10-10 16:01:56 +0200

    test

--- demoGit/src/airial/git/demo/DemoSecondFichier.java
+++ demoGit/src/airial/git/demo/DemoSecondFichier.java
@@ -3,6 +3,10 @@ package airial.git.demo;
 import java.util.Calendar;
 
 public class DemoSecondFichier {
+/**
+ * Lancement de la version 2.
+ * @param args
+ */
 public static void main(String[] args) {
 	int date = Calendar.getInstance().get(Calendar.MONTH);
 	switch (date) {
